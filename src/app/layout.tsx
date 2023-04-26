19aee84cb0b670ef9572a371b019aabf79b5e3ac
Pablo George <pablogeokar@hotmail.com>
2023-04-26 15:36:46 -0300

    ajustes de pastas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import { Nunito } from "next/font/google";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 
 import "./globals.css";
-import ClientOnly from "./components/ClientOnly";
-import RegisterModal from "./components/modals/RegisterModal";
-import RentModal from "./components/modals/RentModal";
-import LoginModal from "./components/modals/LoginModal";
-import ToasterProvider from "./providers/ToasterProvider";
-import { getCurrentUser } from "./actions/getCurrentUser";
+import ClientOnly from "../components/ClientOnly";
+import RegisterModal from "../components/modals/RegisterModal";
+import RentModal from "../components/modals/RentModal";
+import LoginModal from "../components/modals/LoginModal";
+import ToasterProvider from "../providers/ToasterProvider";
+import { getCurrentUser } from "../actions/getCurrentUser";
 
 export const metadata = {
   title: "Airbnb",
